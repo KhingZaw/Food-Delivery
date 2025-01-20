1c37a92771e034e1d98e024b55a5904df96c7463
KhingZaw <139222945+KhingZaw@users.noreply.github.com>
2025-01-21 00:18:54 +0630

    [themeData save using sharedPreferences]

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,22 +1,25 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/services/auth/auth_gate.dart';
+import 'package:food_delivery/themes/theme_provider.dart';
 import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final imagePart = Provider.of<ThemeProvider>(context);
     return AnimatedSplashScreen(
       splash: Center(
         child: LottieBuilder.asset(
-          "assets/images/logo/Animation_light - 1737205427052.json",
+          imagePart.logoPath,
           fit: BoxFit.cover,
         ),
       ), // Add your logo path
 
-      backgroundColor: Colors.black,
+      backgroundColor: Theme.of(context).colorScheme.surface,
       splashTransition: SplashTransition.fadeTransition,
       splashIconSize: 250, // Size of the splash image
       duration: 4000, // Duration in milliseconds
--- lib/themes/theme_provider.dart
+++ lib/themes/theme_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:food_delivery/themes/dark_mode.dart';
 import 'package:food_delivery/themes/light_mode.dart';
 
@@ -9,15 +10,19 @@ class ThemeProvider with ChangeNotifier {
 
   bool get isDarkMode => _themeData == darkMode;
 
-// Default to light mode
   String _logoPath = "assets/images/logo/Animation_dark - 1737205427052.json";
 
   // Getter for image path
   String get logoPath => _logoPath;
 
+  ThemeProvider() {
+    _loadTheme(); // Load theme preference when the provider is created
+  }
+
   set themeData(ThemeData themeData) {
     _themeData = themeData;
     notifyListeners();
+    _saveTheme(themeData == darkMode); // Save theme preference when it changes
   }
 
   void toggleTheme() {
@@ -28,5 +33,25 @@ class ThemeProvider with ChangeNotifier {
       themeData = lightMode;
       _logoPath = "assets/images/logo/Animation_dark - 1737205427052.json";
     }
+  }
+
+  Future<void> _loadTheme() async {
+    SharedPreferences themeSharedPreference =
+        await SharedPreferences.getInstance();
+    bool isDark = themeSharedPreference.getBool('isDarkMode') ??
+        false; // Default to false
+    if (isDark) {
+      themeData = darkMode;
+      _logoPath = "assets/images/logo/Animation_light - 1737205427052.json";
+    } else {
+      themeData = lightMode;
+      _logoPath = "assets/images/logo/Animation_dark - 1737205427052.json";
+    }
+  }
+
+  Future<void> _saveTheme(bool isDark) async {
+    SharedPreferences themeSharedPreference =
+        await SharedPreferences.getInstance();
+    themeSharedPreference.setBool('isDarkMode', isDark);
   }
 }
