21081c91ff3e8eecd335c6421300947a01595ad5
KhingZaw <139222945+KhingZaw@users.noreply.github.com>
2025-01-23 03:06:43 +0630

    [shopping icon use badges]

--- lib/components/my_sliver_app_bar.dart
+++ lib/components/my_sliver_app_bar.dart
@@ -1,5 +1,8 @@
+import 'package:badges/badges.dart' as badges;
 import 'package:flutter/material.dart';
+import 'package:food_delivery/models/restaurant.dart';
 import 'package:food_delivery/screens/cart_screen.dart';
+import 'package:provider/provider.dart';
 
 class MySliverAppBar extends StatelessWidget {
   final Widget child;
@@ -8,40 +11,64 @@ class MySliverAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverAppBar(
-      expandedHeight: 340,
-      collapsedHeight: 120,
-      floating: false,
-      pinned: true,
-      actions: [
-        //card button
-        IconButton(
-          onPressed: () {
-            //go to cart screen
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => CartScreen(),
+    return Consumer<Restaurant>(builder: (context, restaurant, _) {
+      int userCart = restaurant.cart.length;
+      return SliverAppBar(
+        expandedHeight: 340,
+        collapsedHeight: 120,
+        floating: false,
+        pinned: true,
+        actions: [
+          // Cart button with badge
+          badges.Badge(
+            position: badges.BadgePosition.topEnd(top: 0, end: 7),
+            badgeStyle: badges.BadgeStyle(
+              badgeColor: Colors.blue,
+              shape: badges.BadgeShape.circle,
+              padding: const EdgeInsets.all(6),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            badgeAnimation: const badges.BadgeAnimation.slide(
+              animationDuration: Duration(milliseconds: 200),
+            ),
+            showBadge: userCart > 0,
+            badgeContent: Text(
+              '$userCart',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 9,
               ),
-            );
-          },
-          icon: Icon(Icons.shopping_cart_outlined),
-        ),
-      ],
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      foregroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: Text("Sunset Diner"),
-      centerTitle: true,
-      flexibleSpace: FlexibleSpaceBar(
-        title: title,
+            ),
+            //card button
+            child: IconButton(
+              icon: Icon(Icons.shopping_cart_outlined),
+              onPressed: () {
+                //go to cart screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CartScreen(),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Sunset Diner"),
         centerTitle: true,
-        background: Padding(
-          padding: EdgeInsets.only(bottom: 50.0),
-          child: child,
+        flexibleSpace: FlexibleSpaceBar(
+          title: title,
+          centerTitle: true,
+          background: Padding(
+            padding: EdgeInsets.only(bottom: 50.0),
+            child: this.child,
+          ),
+          titlePadding: EdgeInsets.only(left: 0, right: 0, top: 0),
+          expandedTitleScale: 1,
         ),
-        titlePadding: EdgeInsets.only(left: 0, right: 0, top: 0),
-        expandedTitleScale: 1,
-      ),
-    );
+      );
+    });
   }
 }
